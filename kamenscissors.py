0d0df5d81c76e2923b89f8912dbfe0b6e5dc43de
Kliment <72714002+EstonecOG@users.noreply.github.com>
2021-11-09 13:51:43 +0200

    Add files via upload

---
+++ kamenscissors.py
@@ -0,0 +1,14 @@
+from keyboard import*
+from random import*
+from modul1 import*
+from modul2 import*
+v1=["Kivi", "Kaarid", "Paber"]
+v2=["Kivi", "Kaarid", "Paber"]
+
+m=start()
+if m==1:
+    modul2(v1,v2)
+elif m==2:
+    while 1:
+        pass
+
